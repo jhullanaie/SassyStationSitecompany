8c6e8f183ed5af91bbffd49ed4884bc8a4498c35
kuoblured <kuoblured@gmail.com>
2025-03-14 10:40:35 +0800

    10:40AM

--- Review.js
+++ Review.js
@@ -3,11 +3,11 @@ let currentSlide = 0;
 function moveSlide(direction) {
     const slides = document.querySelectorAll('.carousel-item');
     const totalSlides = slides.length;
-
-    // Update current slide index
+ 
+   
     currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
 
-    // Calculate the offset for the carousel
-    const offset = -currentSlide * 100; // Move left by 100% of the current slide
+   
+    const offset = -currentSlide * 100;
     document.querySelector('.carousel-inner').style.transform = `translateX(${offset}%)`;
 }
